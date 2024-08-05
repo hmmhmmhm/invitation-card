c4ba1a6653537656b8215ff704b854d43253126e
hmmhmmhm <hmmhmmhm@naver.com>
2024-08-05 11:17:04 +0900

    fix: 숫자 오류 수정

--- components/CommentSection.tsx
+++ components/CommentSection.tsx
@@ -242,7 +242,7 @@ export default function CommentSection() {
                         <span>{entry.user_from}</span>
                       </>
                     )}
-                    {entry.score && (
+                    {entry.score && entry.score > 0 && (
                       <>
                         <span className="mx-4 text-[#F68DA7]">/</span>
                         <span>{entry.score}점</span>
